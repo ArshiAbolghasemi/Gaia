a41f37e543419aea7ac0492074ce210c2bf867ff
Aboli <arshiabolghasemi@gmail.com>
2026-05-15 19:41:43 +0330

    fix note book pcmci + cmi gpu check issues

--- src/pcmci/cmi.py
+++ src/pcmci/cmi.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
 import logging
+import os
 from importlib import import_module
 from typing import Any, cast
+
+os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("VECLIB_MAXIMUM_THREADS", "1")
 
 import faiss
 import numpy as np
